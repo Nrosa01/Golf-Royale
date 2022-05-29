4666ccfc3cf356f5995072cc6d27654d90051795
nrosa01 <nrosa01@ucm.es>
2022-05-29 16:01:57 +0200

    Inicializacion del repositorio con SDL

---
+++ src/SDLUtils/SDLApp.h
@@ -0,0 +1,20 @@
+#include <SDL2/SDL.h>
+#include <vector>
+
+class SDLEntity;
+
+class SDLApp
+{
+public:
+    SDLApp(int width, int height, const char* title);
+    ~SDLApp();
+
+    void update(float deltaTime);
+    void render();
+
+private:
+    SDL_Window* window;
+    SDL_Renderer* renderer;
+
+    std::vector<SDLEntity*> entities;
+};
