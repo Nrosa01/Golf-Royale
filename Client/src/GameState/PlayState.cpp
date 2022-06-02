b7f0b4c2eb70fe36f393c389d014abc96331e5ab
Nicolas Rosa Caballero <nrosa01@ucm.es>
2022-06-02 20:05:32 +0200

    Añadido sistema obstaculos

--- Client/src/GameState/PlayState.cpp
+++ Client/src/GameState/PlayState.cpp
@@ -1,12 +1,15 @@
 #include "PlayState.h"
 #include "../EC/Entity.h"
 #include "../EC/Components/Ball.h"
+#include "../EC/Components/Transform.h"
+#include "../EC/Components/Renderer.h"
 #include "../EC/Components/BallDirection.h"
 #include "../EC/Components/Transitioner.h"
 #include "../EC/Components/TextRenderer.h"
 #include "../SDLUtils/SDLApp.h"
+#include <sstream>
 
-PlayState::PlayState(SDLApp *app, std::string enemyNick, bool isMaster) : GameState(app)
+PlayState::PlayState(SDLApp *app, std::string enemyNick, bool isMaster) : GameState(app), currentLevel(0)
 {
     createEntity(Vector2D(app->getWidth() / 2, app->getHeight() / 2), Vector2D(1, 1), "menuBg");
     Entity *fg = createEntity(Vector2D(app->getWidth() / 2, app->getHeight() / 2), Vector2D(1, 1), "GolfRoyaleBg");
@@ -19,16 +22,16 @@ PlayState::PlayState(SDLApp *app, std::string enemyNick, bool isMaster) : GameSt
     enemyBall->AddComponent(new Ball(false, !isMaster));
 
     // UI
-    Entity *playerNick = createEntity(Vector2D(app->getWidth() / 2 - 50, 50), Vector2D(0.15f, 0.4f), "button");
+    Entity *playerNick = addUI(Vector2D(app->getWidth() / 2 - 50, 50), Vector2D(0.15f, 0.4f), "button");
     playerNick->AddComponent(new TextRenderer(app->getPlayerName(), "toonFont", 24));
 
-    Entity *enemyNickEnt = createEntity(Vector2D(app->getWidth() / 2 + 50, 50), Vector2D(0.15f, 0.4f), "button");
+    Entity *enemyNickEnt = addUI(Vector2D(app->getWidth() / 2 + 50, 50), Vector2D(0.15f, 0.4f), "button");
     enemyNickEnt->AddComponent(new TextRenderer(enemyNick, "toonFont", 24));
 
-    Entity *playerScore = createEntity(Vector2D(app->getWidth() / 2 - 50, 80), Vector2D(0.075f, 0.2f), "button");
+    Entity *playerScore = addUI(Vector2D(app->getWidth() / 2 - 50, 80), Vector2D(0.075f, 0.2f), "button");
     playerScore->AddComponent(new TextRenderer("0", "toonFont", 16));
 
-    Entity *enemyScore = createEntity(Vector2D(app->getWidth() / 2 + 50, 80), Vector2D(0.075f, 0.2f), "button");
+    Entity *enemyScore = addUI(Vector2D(app->getWidth() / 2 + 50, 80), Vector2D(0.075f, 0.2f), "button");
     enemyScore->AddComponent(new TextRenderer("0", "toonFont", 16));
 
     // Transitioners
@@ -39,6 +42,14 @@ PlayState::PlayState(SDLApp *app, std::string enemyNick, bool isMaster) : GameSt
     addTransitioner(enemyNickEnt);
     addTransitioner(playerScore);
     addTransitioner(enemyScore);
+
+    loadLevel(currentLevel);
+}
+
+PlayState::~PlayState()
+{
+    deleteObstacles();
+    deleteUI();
 }
 
 void PlayState::onStateExit()
@@ -52,4 +63,113 @@ void PlayState::receiveNetworkMessage(NetworkMessage &msg)
     GameState::receiveNetworkMessage(msg);
     if (msg.type == PLAYER_DISCONNECTED)
         startExitTransitionTimer(popState);
+}
+
+void PlayState::loadLevel(int level)
+{
+    deleteObstacles();
+    std::string levelPath = "assets/levels";
+    std::string levelName = "level" + std::to_string(level);
+
+    struct obstacle
+    {
+        Vector2D pos;
+        Vector2D scale;
+    };
+    std::vector<obstacle> obstacles;
+
+    std::ifstream levelFile(levelPath + "/" + levelName + ".dat");
+
+    if (levelFile.is_open())
+    {
+        std::string line;
+        while (std::getline(levelFile, line))
+        {
+            std::stringstream ss(line);
+            std::string type;
+            obstacle o;
+            ss >> o.pos.x >> o.pos.y >> o.scale.x >> o.scale.y;
+            obstacles.push_back(o);
+        }
+    }
+    else
+    {
+        std::cout << "Could not open level file: " << levelPath + "/" + levelName + ".dat" << std::endl;
+    }
+
+    int width = app->getWidth();
+    for (auto &o : obstacles)
+    {
+        addObstacle(o.pos, o.scale, "obstacle1");
+        addObstacle(o.pos + Vector2D(width / 2, 0), o.scale, "obstacle2");
+    }
+
+    for (auto &o : this->obstacles)
+        o->init();
+}
+
+Entity *PlayState::addObstacle(Vector2D pos, Vector2D scale, string textureName)
+{
+    Entity *e = new Entity(pos.x, pos.y, app);
+    e->GetTransform()->GetScale() = scale;
+    if (!textureName.empty())
+        e->AddComponent(new Renderer(app->getTexture(textureName)));
+    obstacles.push_back(e);
+
+    return e;
+}
+
+Entity *PlayState::addUI(Vector2D pos, Vector2D scale, string textureName)
+{
+    Entity *e = new Entity(pos.x, pos.y, app);
+    e->GetTransform()->GetScale() = scale;
+    if (!textureName.empty())
+        e->AddComponent(new Renderer(app->getTexture(textureName)));
+    ui.push_back(e);
+
+    return e;
+}
+
+void PlayState::render()
+{
+    GameState::render();
+
+    for (auto &o : obstacles)
+        o->render();
+
+    for (auto &u : ui)
+        u->render();
+}
+
+void PlayState::deleteObstacles()
+{
+    for (auto &o : obstacles)
+        delete o;
+    obstacles.clear();
+}
+
+void PlayState::deleteUI()
+{
+    for (auto &o : ui)
+        delete o;
+    ui.clear();
+}
+
+void PlayState::init()
+{
+    GameState::init();
+
+    for (auto &o : ui)
+        o->init();
+}
+
+void PlayState::update(float dt)
+{
+    GameState::update(dt);
+
+    for (auto &o : ui)
+        o->update(dt);
+
+    for (auto &o : ui)
+        o->lateUpdate(dt);
 }
--- Client/src/GameState/PlayState.h
+++ Client/src/GameState/PlayState.h
@@ -2,14 +2,29 @@
 #define PLAYSTATE_H
 
 #include "GameState.h"
+#include <vector>
+class Entity;
 
 class PlayState : public GameState
 {
 public:
     PlayState(SDLApp *app, std::string enemyNick = "Test", bool isMaster = true);
-    virtual ~PlayState(){};
-    virtual void receiveNetworkMessage(NetworkMessage& msg);
+    virtual ~PlayState();
+    virtual void init();
+    virtual void update(float dt);
+    virtual void render();
+    virtual void receiveNetworkMessage(NetworkMessage &msg);
     virtual void onStateExit();
+    void deleteObstacles();
+    void deleteUI();
+
+private:
+    int currentLevel;
+    std::vector<Entity *> obstacles;
+    std::vector<Entity *> ui;
+    void loadLevel(int level);
+    Entity *addObstacle(Vector2D pos, Vector2D scale, string textureName);
+    Entity *addUI(Vector2D pos, Vector2D scale, string textureName);
 };
 
 #endif
